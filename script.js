561bd2c384a3f6f24004551f06b81305369fdc78
RockyDEV <87176716+Gamer5595@users.noreply.github.com>
2021-10-14 09:20:18 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let person1 = new Person("Jack", 20);
+let person2 = new Person("Alan", 24);
+
+console.log(person1, person2);
